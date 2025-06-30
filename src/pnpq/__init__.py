46bb6527ce7de0f666bca171eb6a37dfb733c8c8
Tom Shen <40376288+TomShen1234@users.noreply.github.com>
2025-06-30 23:17:16 +0900

    Revert dynamic version

--- src/pnpq/__init__.py
+++ src/pnpq/__init__.py
@@ -1,16 +1,18 @@
 """Python library to control hardware used in quantum optical testbeds."""
 
-from pathlib import Path
-from typing import Optional
+__version__ = "0.1.1"
+
+# from pathlib import Path
+# from typing import Optional
 
 
-def get_version() -> Optional[str]:
-    """Get the version number from version.txt."""
-    version_file = Path(__file__).parent / "version.txt"
-    if version_file.exists():
-        with open(version_file, "r", encoding="utf-8") as f:
-            return f.read().strip()
-    return None
+# def get_version() -> Optional[str]:
+#     """Get the version number from version.txt."""
+#     version_file = Path(__file__).parent / "version.txt"
+#     if version_file.exists():
+#         with open(version_file, "r", encoding="utf-8") as f:
+#             return f.read().strip()
+#     return None
 
 
-__version__ = get_version()
+# __version__ = get_version()
